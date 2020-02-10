fdd670335a6c84afe345c4b2e04fac336eef50e7
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-10 16:32:11 -0500

    set up state in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const initialData = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todoData: initialData
+    }
+  }
+
+  setTodoData = newTodoData => {
+    this.setState({
+      ...this.state,
+      todoData: newTodoData
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todoData={this.state.todoData} setTodoData={this.setTodoData}/>
+        <TodoForm todoData={this.state.todoData} setTodoData={this.setTodoData}/>
       </div>
     );
   }
